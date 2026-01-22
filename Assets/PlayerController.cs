6eb51ae69cbd641aa8620134f64b06dea7c9a52c
t1ffanyc <tchen1515@gmail.com>
2026-01-22 14:34:44 -0800

    add dash

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,8 +7,10 @@ public class PlayerController : MonoBehaviour
     float movementX;
     float movementY;
     [SerializeField] float speed = 5.0f;
+    [SerializeField] float dashForce = 12f;
     Rigidbody2D rb;
     bool isGrounded;
+    bool isDashing;
     int score = 0;
 
     void Start()
@@ -22,8 +24,12 @@ public class PlayerController : MonoBehaviour
         // float movementDistanceX = movementX * speed * Time.deltaTime;
         // float movementDistanceY = movementY * speed * Time.deltaTime;
         // transform.position = new Vector2(transform.position.x + movementDistanceX, transform.position.y + movementDistanceY);
-        rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
-        if(movementY > 0 && isGrounded)
+        if (!isDashing)
+        {
+            rb.linearVelocity = new Vector2(movementX * speed, rb.linearVelocity.y);
+        }
+
+        if (movementY > 0 && isGrounded)
         {
             rb.AddForce(new Vector2(0, 100));
         }
@@ -36,6 +42,26 @@ public class PlayerController : MonoBehaviour
         movementY = v.y;
         Debug.Log("Movement X = " + movementX);
         Debug.Log("Movement Y = " + movementY);
+    }
+
+    void OnDash(InputValue value)
+    {
+        isDashing = true;
+
+        Vector2 dashDir = new Vector2(movementX, 0);
+
+        if (dashDir == Vector2.zero)
+            dashDir = Vector2.right * transform.localScale.x;
+
+        rb.AddForce(dashDir.normalized * dashForce, ForceMode2D.Impulse);
+        Debug.Log("dash");
+
+        Invoke(nameof(EndDash), 0.15f);
+    }
+
+    void EndDash()
+    {
+        isDashing = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
